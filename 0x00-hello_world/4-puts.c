cd592fb6d14298abf02c6e4bd1b61cc9fac44c26
Kojowest <kojowest2012@gmail.com>
2022-04-08 09:12:49 +0000

    print to screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+   * main - print to screen
+[D[D[D[D*
+[D[D[D[D* Return: zero
+   */
+   int main(void)
+   {
+         char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+         puts(Mystr);
+[D[D[D[D[Dreturn (0);
+   }
